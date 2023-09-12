27aabf3444c39d6de078af03b38ec49bb298df1b
SornchaiTheDev <sornchaithedev@gmail.com>
2023-09-12 22:46:40 +0700

    Add : Lab08 road construction

--- Lab/Lab08/roadConstruction.cpp
+++ Lab/Lab08/roadConstruction.cpp
@@ -1,16 +1,77 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
+
+class Graph
+{
+private:
+  int n;
+  vector<vector<int> > adj;
+  vector<bool> visited;
+
+public:
+  Graph(int n) : n(n), adj(n + 1), visited(n + 1, false) {}
+
+  void addEdge(int v, int w)
+  {
+    adj[v].push_back(w);
+    adj[w].push_back(v);
+  }
+
+  void BFS(int v)
+  {
+    queue<int> q;
+    q.push(v);
+    visited[v] = true;
+
+    while (!q.empty())
+    {
+      int current = q.front();
+      q.pop();
+
+      for (int u : adj[current])
+      {
+        if (!visited[u])
+        {
+          q.push(u);
+          visited[u] = true;
+        }
+      }
+    }
+  }
+
+  int countComponents()
+  {
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+      if (!visited[i])
+      {
+        BFS(i);
+        count++;
+      }
+    }
+    return count;
+  }
+};
 
 int main()
 {
-  int town;
-  int road;
-  cin >> town >> road;
-  for (int i = 0; i < road; i++)
+  int city, roadComplete;
+  cin >> city >> roadComplete;
+
+  Graph g(city);
+
+  for (int i = 0; i < roadComplete; i++)
   {
-    int roadA;
-    int roadB;
-    cin >> roadA >> roadB;
+    int x, y;
+    cin >> x >> y;
+    g.addEdge(x, y);
   }
-  // cout << town << " " << road;
+
+  int count = g.countComponents() - 1;
+  cout << count << endl;
+
+  return 0;
 }
